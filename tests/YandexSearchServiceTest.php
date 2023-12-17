c916e1c939854f25ce25a211005fec4128f6015e
Andrey Panisko <starkeen@gmail.com>
2023-12-17 18:59:49 +0600

    Basic settings for project

---
+++ tests/YandexSearchServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YandexSearchAPI\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class YandexSearchServiceTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
